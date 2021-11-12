8eb051936bc0690f6bb5e5150fe142fbaa0f39f5
samuel santos <samueldasilva2209@gmail.com>
2021-11-12 15:23:54 -0300

    adicionando mensagem de acordo com o peso

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function calcular(){
+    
     let peso = Number(document.getElementById('peso').value);
     let altura = Number(document.getElementById('altura').value);
-    let imc = peso/(altura*altura)
+    let imc = peso/(altura*altura);
     let res= document.getElementById('res');
-    res.innerHTML = imc.toFixed(2)
+    let sit = document.getElementById('sit');
+    let escrever ;
+    if(peso === 0 || altura === 0){
+       alert('Por favor coloque os dados corretamente')
+    }else{
+
+        if(imc < 18.5){
+            escrever = 'Voce está abaixo do peso';
+        }else if ( imc >= 18.5 && imc < 25){
+            escrever = 'Seu peso está normal'
+        }else if ( imc >= 25 && imc < 30){
+            escrever = 'Voce está acima do peso, tome cuidado'
+        }else if (imc >= 30 && imc < 35){
+            escrever = 'Obesidade (Grau 1), consulte um médico'
+        }
+        else if(imc >= 35 && imc < 40){
+            escrever = 'Obesidade Severa (Grau 2)!!'
+
+        }
+        else{
+            escrever = ' Obesidade Mórbida (Grau 3) Voce necessita de um tratamento urgente!!!'
+        }
+        res.innerHTML = imc.toFixed(2);
+        sit.innerHTML = escrever ;
+    }
+    
 }
